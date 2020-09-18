968b7119ce465a89b864999222475e667d3a18d2
caglar celikoz <caglar@caglars-MBP.iconos.be>
2020-09-18 10:00:54 +0200

    add project

---
+++ src/main/java/com/test/springboottestrest/SpringBootTestRestApplication.java
@@ -0,0 +1,13 @@
+package com.test.springboottestrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/springboottestrest/SpringBootTestRestApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.test.springboottestrest;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringBootTestRestApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
